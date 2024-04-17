9404a2605688279d1ad0195162251544c02455da
pgomezunicauca <152555878+pgomezunicauca@users.noreply.github.com>
2024-04-16 20:14:57 -0500

    Add files via upload

---
+++ login-master/target/generated-sources/annotations/com/todocodeacademy/login/logica/Rol_.java
@@ -0,0 +1,18 @@
+package com.todocodeacademy.login.logica;
+
+import com.todocodeacademy.login.logica.Usuario;
+import javax.annotation.processing.Generated;
+import javax.persistence.metamodel.ListAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-04-16T15:16:08", comments="EclipseLink-2.7.9.v20210604-rNA")
+@StaticMetamodel(Rol.class)
+public class Rol_ { 
+
+    public static volatile SingularAttribute<Rol, String> descripcion;
+    public static volatile ListAttribute<Rol, Usuario> listaUsuarios;
+    public static volatile SingularAttribute<Rol, String> nombreRol;
+    public static volatile SingularAttribute<Rol, Integer> id;
+
+}
